bac31736f786533be09194967ffec0ea84017ae4
YakupHadutoglu <yakuphad2004@gmail.com>
2025-11-27 22:48:52 +0300

    fix(auth): add try/catch to requireAuth middleware for safer token verification

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,14 +2,22 @@ import { Request, Response, NextFunction } from 'express';
 import { verifyAccessToken } from 'utils/token';
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
-    const authHeader = req.headers.authorization;
+    try {
+        const authHeader = req.headers.authorization;
 
-    if (!authHeader || !authHeader.startsWith('Bearer ')) return res.status(401).json({ message: 'Unauthorized! No token provided ' });
-    const token = authHeader.split(' ')[1];
-    const decoded = verifyAccessToken(token);
+        if (!authHeader || !authHeader.startsWith('Bearer ')) return res.status(401).json({ message: 'Unauthorized! No token provided ' });
 
-    if (!decoded) return res.status(401).json({ message: 'Unauthorized: on expired token.' });
+        const token = authHeader.split(' ')[1];
+        
+        const decoded = verifyAccessToken(token);
 
-    req.user = decoded;
-    next();
+        if (!decoded) return res.status(401).json({ message: 'Unauthorized: on expired token.' });
+
+        req.user = decoded;
+        next();
+    } catch (error) {
+        console.error('Authentication Error: ', error);
+        return res.status(401).json({ message: 'Unauthorized: on expired token.' });
+    }
+
 }
