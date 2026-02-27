10dfa64212941f8f4eec7ce463c8063446e806a4
YakupHadutoglu <yakuphad2004@gmail.com>
2026-02-27 16:03:51 +0300

    feat(db): create MongoDB time-series model for logs
    Implemented the Log schema using Mongoose.
    - Configured Time-Series settings for grouping by projectId.
    - Set timestamp as the timeField with seconds granularity.
    - Added 30-day TTL index (expireAfterSeconds) for automatic cleanup.
    - Used Schema.Types.Mixed for flexible metadata storage.

---
+++ src/models/Log.ts
@@ -0,0 +1,46 @@
+import { requireAuth } from 'middlewares/auth';
+import mongoose, { Schema, Document } from 'mongoose';
+import { required } from 'zod/v4/core/util.cjs';
+
+
+export interface ILog extends Document {
+    projectId: string;
+    level: 'info' | 'warn' | 'error' | 'fatal' | 'debug';
+    message: string;
+    metadata: any;
+    timestamp: Date;
+}
+
+const LogSchema: Schema = new Schema({
+    projectId: {
+        type: String,
+        required: true,
+        index: true
+    },
+    level: {
+        type: String,
+        required: true,
+        enum: ['info', 'warn', 'error', 'fatal', 'debug']
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    metadata: {
+        type: Schema.Types.Mixed,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        required: true
+
+    }
+}, {
+    timeseries: {
+        timeField: 'timestamp',
+        metaField: 'projectId',
+        granularity: 'seconds'
+    }
+});
+
+export const LogModel = mongoose.model<ILog>('Log', LogSchema);
